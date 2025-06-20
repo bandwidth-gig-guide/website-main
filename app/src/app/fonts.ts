1bc520a02a663cee302bc4b0554f16c59e984734
MatthewLeigh <matthewcross97@gmail.com>
2025-06-20 11:37:29 +1000

    chore: move fonts to dedicated file

---
+++ app/src/app/fonts.ts
@@ -0,0 +1,12 @@
+import { Geist, Geist_Mono } from "next/font/google";
+
+export const font_primary = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+export const font_secondary = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
