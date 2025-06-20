ca742ac906e6e73f90105ff3a661905431a56e9f
MatthewLeigh <matthewcross97@gmail.com>
2025-06-20 12:57:22 +1000

    fix: update import route for fonts

--- app/layouts/layout.tsx
+++ app/layouts/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { font_primary, font_secondary } from "@/styles/fonts";
+import { font_primary, font_secondary } from "../styles/fonts";
 import "./global.css"
 
 export default function RootLayout({
