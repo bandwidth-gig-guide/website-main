2ce1dc865ccc10888f44e86ea5f79eca493af717
MatthewLeigh <matthewcross97@gmail.com>
2025-06-30 11:15:05 +1000

    feat: add routing to artist and venue pages

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,30 +1,20 @@
 import Link from "next/link";
-import React, { useState, useEffect } from "react"
-import axios from "axios"
-import { Event } from "../types/Event"
+
+const routes = [
+  { href: "/event", label: "Event" },
+  { href: "/artist", label: "Artist" },
+  { href: "/venue", label: "Venue" },
+];
 
 const Home = () => {
-  const [events, setEvents] = useState<Event[]>([]);
-  
-  useEffect(() => {
-    axios.get('http://localhost:80/event/')
-      .then(response => {
-        setEvents(response.data);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }, []);
 
   return (
     <div>
       <h1>Home</h1>
-      <Link href="/event">Event</Link>
-      <h2>Test</h2>
       <ul>
-        {events.map(event => (
-          <li key={event.EventID}>
-            {event.Title}
+        {routes.map(route => (
+          <li>
+            <Link key={route.href} href={route.href}>{route.label}</Link>
           </li>
         ))}
       </ul>
