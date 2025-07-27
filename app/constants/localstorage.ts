25f56ccd349f0fed113b7824921bac7b34501ac3
MatthewLeigh <matthewcross97@gmail.com>
2025-07-27 22:23:10 +1000

    feat: add localstorage keys

---
+++ app/constants/localstorage.ts
@@ -0,0 +1,9 @@
+// All
+export const FILTER_TAGS_KEY = "filterTags";
+
+// Artist
+export const FILTER_ARTIST_TYPE_KEY = "filterArtistType";
+export const FILTER_ARTIST_NAME_KEY = "filterArtistName";
+export const FILTER_ARTIST_CITY_KEY = "filterArtistCity";
+export const FILTER_ARTIST_COUNTRY_KEY = "filterArtistCountry";
+export const FILTER_ARTIST_UPCOMING_KEY = "filterArtistHasUpcomingPerformance";
