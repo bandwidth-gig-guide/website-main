bf88fe898c2dee5b752a147cbd8c7fcbdb554c1a
MatthewLeigh <matthewcross97@gmail.com>
2025-07-27 22:23:38 +1000

    feat: add FilterBase component

---
+++ app/components/Filter/FilterBase/FilterBase.tsx
@@ -0,0 +1,10 @@
+import styles from './FilterBase.module.css'
+import React from 'react'
+
+const FilterBase = () => {
+  return (
+    <div className={styles.wrapper}>FilterBase</div>
+  )
+}
+
+export default FilterBase
