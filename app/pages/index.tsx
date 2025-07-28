8dca37dfda6a7413071543465f8b76f665920bf4
MatthewLeigh <matthewcross97@gmail.com>
2025-07-28 17:40:48 +1000

    feat: add card grids to the homepage

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,19 +1,52 @@
 // React / Next
-import React from "react"
+import React, {useState, useEffect} from "react"
 import Head from 'next/head';
 
-import SectionHeader from "../components/SectionHeader/SectionHeader";
+// External libraries
+import axios from 'axios';
+import camelcaseKeys from 'camelcase-keys';
 
-const routes = [
-  { title: "Events", route: "/event" },
-  { title: "Artists", route: "/artist" },
-  { title: "Venues", route: "/venue" },
-  { title: "Articles" },
-  { title: "Teachers" },
-  { title: "Stores" },
-];
+// Components
+import SectionHeader from "../components/SectionHeader/SectionHeader";
+import CardGrid from "../components/CardGrid/CardGrid";
+
+// Config
+import apiUrl from '../api.config';
+import { CardGridType } from "@/types/enums/CardGridType";
+
 
 const Home = () => {
+
+  // State
+  const [events, setEvents] = useState<uuid[]>([]);
+  const [artists, setArtists] = useState<uuid[]>([]);
+  const [venues, setVenues] = useState<uuid[]>([]);
+
+  // Fetch IDs
+  useEffect(() => {
+    const fetch = async (type: string) => {
+      try {
+        const url = `${apiUrl}/${type}`
+        const response = await axios.get(url);
+
+        switch (type) {
+          case 'event': setEvents(camelcaseKeys(response.data, { deep: true })); break;
+          case 'artist': setArtists(camelcaseKeys(response.data, { deep: true })); break;
+          case 'venue': setVenues(camelcaseKeys(response.data, { deep: true })); break;
+          default: break;
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetch('event');
+    fetch('artist');
+    fetch('venue');
+
+  }, []);
+
+
   return (
     <>
       <Head>
@@ -21,9 +54,21 @@ const Home = () => {
         <meta name="description" content="Welcome to my website" />
       </Head>
 
-      {routes.map(route => (
-        <SectionHeader title={route.title} route={route.route || undefined} />
-      ))}
+      <div style={{ marginBottom: 60 }}>
+        <SectionHeader title='Events' route='/event' />
+        <CardGrid eventIds={events} cardGridType={CardGridType.Grid}/>
+      </div>
+      
+      <div style={{ marginBottom: 60 }}>
+        <SectionHeader title='Artists' route='/artist' />
+        <CardGrid  artistIds={artists} cardGridType={CardGridType.Grid}/>
+      </div>
+
+      <div style={{ marginBottom: 60 }}>
+        <SectionHeader title='Venues' route='/venue' />
+        <CardGrid  venueIds={venues} cardGridType={CardGridType.Grid}/>
+      </div>
+      
     </>
   );
 };
