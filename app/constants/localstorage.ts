3fdf14469a7414144599dc4a94f9d10973ccdae9
MatthewLeigh <matthewcross97@gmail.com>
2025-07-28 16:41:28 +1000

    feat: add localstorage keys for events

--- app/constants/localstorage.ts
+++ app/constants/localstorage.ts
@@ -7,3 +7,11 @@ export const FILTER_ARTIST_NAME_KEY = "filterArtistName";
 export const FILTER_ARTIST_CITY_KEY = "filterArtistCity";
 export const FILTER_ARTIST_COUNTRY_KEY = "filterArtistCountry";
 export const FILTER_ARTIST_UPCOMING_KEY = "filterArtistHasUpcomingPerformance";
+
+// Event
+export const FILTER_EVENT_NAME_KEY = "filterEventName";
+export const FILTER_EVENT_STATECODE_KEY = "filterEventStateCode";
+export const FILTER_EVENT_CITY_KEY = "filterEventCity";
+export const FILTER_EVENT_MAX_PRICE_KEY = "filterEventMaxPrice";
+export const FILTER_EVENT_TYPE_KEY = "filterEventType";
+export const FILTER_EVENT_DATE_KEY = "filterEventDate";
