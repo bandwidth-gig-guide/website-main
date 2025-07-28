84d0e211915036fb263f281abdeba572115837d1
MatthewLeigh <matthewcross97@gmail.com>
2025-07-28 21:09:44 +1000

    feat: add skeleton for card loading

--- app/components/Card/CardLoading/CardLoading.tsx
+++ app/components/Card/CardLoading/CardLoading.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import styles from './CardLoading.module.css'
 
 const CardLoading = () => {
   return (
-    <div>CardLoading</div>
+    <div className={styles.wrapper}>
+			<div className={styles.imgWrapper} />
+			<div className={styles.text}>
+				<div className={styles.small50} />
+				<div className={styles.small10} />
+			</div>
+			<div className={styles.text}>
+				<div className={styles.large80} />
+			</div>			
+      <div className={styles.text}>
+				<div className={styles.small25} />
+			</div>	
+    </div>
   )
 }
 
