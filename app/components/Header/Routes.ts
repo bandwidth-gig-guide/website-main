e69227647943eb475309de331484e899422472bd
MatthewLeigh <matthewcross97@gmail.com>
2025-07-28 18:32:13 +1000

    feat: hide pending routes

--- app/components/Header/Routes.ts
+++ app/components/Header/Routes.ts
@@ -2,8 +2,8 @@ export const routes = [
     { label: "Events", href: "/event" },
     { label: "Artists", href: "/artist" },
     { label: "Venues", href: "/venue" },
-    { label: "Articles", href: "/" },
-    { label: "Teachers", href: "/" },
-    { label: "Stores", href: "/" },
-    { label: "About", href: "/" },
+    // { label: "Articles", href: "/" },
+    // { label: "Teachers", href: "/" },
+    // { label: "Stores", href: "/" },
+    // { label: "About", href: "/" },
 ];
