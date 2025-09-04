3ed8174a9419984066f38a79dbcc95f33d2da9c3
MatthewLeigh <matthewcross97@gmail.com>
2025-09-05 00:17:31 +1000

    fix: update address and upcoming events display in CardVenue component

--- app/components/Card/CardVenue/CardVenue.tsx
+++ app/components/Card/CardVenue/CardVenue.tsx
@@ -46,13 +46,25 @@ const CardVenue: React.FC<Props> = ({ venueId }) => {
 	if (isLoading) return <CardLoading />
 	if (isError) return <div>Error loading venue.</div>
 
+	let addressString = `${venue.streetAddress ?? ''}${venue.streetAddress ? ',' : ''} ${venue.city ?? ''} ${venue.stateCode ?? ''} ${venue.postCode ?? ''}`
+
+	let upcomingEventsString = ""
+	switch (venue.upcomingEventCount) {
+		case 1:
+			upcomingEventsString = "1 Upcoming Event"
+			break
+		default:
+			upcomingEventsString = `${venue.upcomingEventCount} Upcoming Events`
+			break
+	}
+
 	return (
 		<div>
 			<Link href={`/venue/${venueId}`}>
 				<CardBase
-					topLeft={`${venue.streetAddress}, ${venue.city} ${venue.stateCode} ${venue.postcode}`}
+					topLeft={addressString}
 					title={venue.title}
-					bottom={`${venue.upcomingEventCount} Upcoming Events`}
+					bottom={upcomingEventsString}
 					imgUrl={hasImage && venue.imageUrl ? venue.imageUrl : ''}
 				/>
 			</Link>
