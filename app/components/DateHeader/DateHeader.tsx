efb4d1ca0ac164413bbdc739768dc194c393bbfd
MatthewLeigh <matthewcross97@gmail.com>
2025-09-05 10:36:13 +1000

    fix: update padding for titleWrapper in DateHeader component

--- app/components/DateHeader/DateHeader.tsx
+++ app/components/DateHeader/DateHeader.tsx
@@ -6,6 +6,7 @@ const SectionHeader: React.FC<Props> = ({ date }) => {
 	return (
 			<div className={styles.wrapper}>
 				<div className={styles.titleWrapper}>
+						{/* <div className={styles.icon}></div> */}
 						<h2>{date}</h2>
 				</div>
 			</div>
