ed8a8b77d1eef194d5e0514c744017118d5cd3d9
MatthewLeigh <matthewcross97@gmail.com>
2025-09-05 10:36:19 +1000

    feat: add pagination and limit to CardGrid in Artist and Venue components

--- app/pages/artist/index.tsx
+++ app/pages/artist/index.tsx
@@ -22,7 +22,7 @@ const Artist = () => {
         <Hero />
         <SectionHeader title="Artists" />
         <FilterArtist setArtistIds={setArtistIds}/>
-        <CardGrid artistIds={artistIds} cardGridType={CardGridType.Grid} />
+        <CardGrid artistIds={artistIds} cardGridType={CardGridType.Grid} isPaginated={true} limit={5}/>
       </div>
     </>
   );
