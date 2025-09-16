9b0031df5f1e434a4262677253bce0d920c61049
MatthewLeigh <matthewcross97@gmail.com>
2025-09-16 23:03:12 +1000

    feat: add message when there are no upcoming events

--- app/components/UpcomingEvents/UpcomingEvents.tsx
+++ app/components/UpcomingEvents/UpcomingEvents.tsx
@@ -7,16 +7,39 @@ interface Props {
   eventIds?: uuid[];
 }
 
+const message = `
+  No events on record. We keep our records as accurate as possible, but we're not always up-to-date, 
+  and we don't track every venue in Melbourne (yet!) Check out the socials and official postings for 
+  the latest updates and events we might have missed!
+`
+
 const UpcomingEvents: React.FC<Props> = ({ eventIds }) => {
   return (
     <div className={styles.wrapper}>
       <SectionHeader title='Upcoming Events' route='/event' />
       <CardGrid eventIds={eventIds} limit={8} isPaginated={true} />
+      {eventIds?.length === 0 &&
+        <>
+          <p className={styles.noEventsMessage}>
+            No events on record.
+          </p>
+          <div className={styles.disclaimerWrapper}>
+          <p>
+            We keep our records as accurate as possible, but we're not always up-to-date, and we 
+            don't track every venue in Melbourne (yet!)
+          </p>
+          <p>
+            Check out the socials and official postings for the latest updates and events we might 
+            have missed!
+          </p>
+          </div>
+        </>
+      }
       {/* <div className={styles.disclaimerWrapper}>
         <p className={styles.disclaimer}>We keep our records as accurate as possible, but we're not always up-to-date, and we don't track every venue in Melbourne (yet!)</p>
         <p className={styles.disclaimer}>Check out the socials and official postings for the latest updates and events we might have missed!</p>
       </div> */}
-    </div>
+    </div >
   );
 };
 
