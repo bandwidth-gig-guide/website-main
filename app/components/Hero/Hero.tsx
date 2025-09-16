cbe862f68a536aa124fa8ed97532ebce84e14adb
MatthewLeigh <matthewcross97@gmail.com>
2025-09-17 09:25:07 +1000

    feat: clicking hero links to about page

--- app/components/Hero/Hero.tsx
+++ app/components/Hero/Hero.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Link from "next/link";
 import styles from "./Hero.module.css";
 
 const artwork = {
-  artwork_21_7: "./heros/0001-21x7.png",
-  artwork_24_36: "", 
+  artwork_21x7: "./heros/0001-21x7.png",
+  artwork_24x36: "",
   artistName: "Matthew Cross",
   artistCompany: "Freelance Graphic Designer",
   artistCompanyUrl: "http://localhost/about",
@@ -13,9 +14,11 @@ const artwork = {
 const Hero = () => {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.artworkWrapper}>
-        <img src={artwork.artwork_21_7} alt="" />
-      </div>
+      <Link href="/about">
+        <div className={styles.artworkWrapper}>
+          <img src={artwork.artwork_21x7} alt={`${artwork.artistName}, ${artwork.artistCompany}`} />
+        </div>
+      </Link>
       <a className={styles.artist} href={artwork.artistCompanyUrl} >
         <p><strong>Artwork by {artwork.artistName}</strong></p>
         <p><em>{artwork.artistCompany}, {artwork.artistLocation}</em></p>
