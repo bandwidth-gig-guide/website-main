78746fb0e9693d540286faa6f38f129c155e7168
MatthewLeigh <matthewcross97@gmail.com>
2025-09-16 23:02:40 +1000

    feat: add tickets to header for events

--- app/components/PageHeader/PageHeader.tsx
+++ app/components/PageHeader/PageHeader.tsx
@@ -5,30 +5,38 @@ interface Props {
 	title: string;
 	pageType: PageType;
 	isFeatured?: boolean;
-	isFreeEvent?: boolean;
 	subtitle?: string;
+	getTicketsUrl?: url;
+	minTicketPrice?: number | null;
 };
 
-const PageHeader: React.FC<Props> = ({ title, pageType, isFeatured, isFreeEvent, subtitle }) => {
+const PageHeader: React.FC<Props> = ({ title, pageType, isFeatured,  subtitle, getTicketsUrl, minTicketPrice }) => {
 
 	const icon = pageType === "venue" ? '/location-pin.svg' : "";
 	const pageTypeUppercase = pageType.charAt(0).toUpperCase() + pageType.slice(1)
 
-	const formattedTopRow = isFeatured
-		? `| Featured ${pageTypeUppercase}`
-		: isFreeEvent
-			? "| Free Event"
-			: `| ${pageTypeUppercase}`;
+	const recordType = minTicketPrice === 0
+			? `| Free Event`
+			: isFeatured
+				? `| Featured ${pageTypeUppercase}`
+				: `| ${pageTypeUppercase}`;
+
+	const priceString = minTicketPrice === 0 || minTicketPrice === null
+		? `Get Tickets`
+		: `Tickets from $${minTicketPrice}`
 
 	return (
 		<div className={styles.wrapper}>
-			<p className={isFeatured || isFreeEvent ? styles.accentColor : styles.topRow}>
-				{formattedTopRow}
+			<p className={styles.topRow}>
+				<span className={isFeatured || minTicketPrice === 0 ? styles.accentColor : ""}>{recordType}</span>
 			</p>
-			<h2>{title}</h2>
+			<div className={styles.middleRow}>
+				<h2>{title}</h2>
+				{getTicketsUrl && <button onClick={() => window.open(getTicketsUrl, '_blank')}>{priceString}</button>}
+			</div>
 			{subtitle &&
 				<div className={styles.bottomRow}>
-					<img src={icon} /> {subtitle}
+					{icon && <img src={icon} />}{subtitle}
 				</div>
 			}
 		</div>
