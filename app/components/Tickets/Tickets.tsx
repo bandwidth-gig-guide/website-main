de787f3b1901811118adc0309592ddfd6f4798b0
MatthewLeigh <matthewcross97@gmail.com>
2025-09-16 14:23:24 +1000

    feat: add ticket prices with temp styling

--- app/components/Tickets/Tickets.tsx
+++ app/components/Tickets/Tickets.tsx
@@ -8,10 +8,8 @@ interface Props {
   originalPostUrl: url;
 }
 
-function formatTicketString(price: EventPrice): string {
-
-  const priceString = price.price === 0 ? "Free Entry" : `$${price.price}`
-  return `${price.ticketType} | ${priceString}`;
+function formatTicketPrice(price: EventPrice): string {
+  return price.price === 0 ? "Free Entry" : `$${price.price}`
 }
 
 const Tickets: React.FC<Props> = ({ prices, ticketSaleUrl, originalPostUrl }) => {
@@ -20,19 +18,24 @@ const Tickets: React.FC<Props> = ({ prices, ticketSaleUrl, originalPostUrl }) =>
       <div className={styles.topRow}>
         <a className={styles.button} href={ticketSaleUrl} target='_blank'>Get Tickets</a>
         <div className={styles.spacer} />
-        <p className={styles.disclaimer}>
-          * This event may be 18+. Information presented on this page may be false or outdated.
-          Always check with the <a href={originalPostUrl}>official listing</a> before purchasing
-          tickets for any event.
-        </p>
+        <ul className={styles.pricesList}>
+          {prices.map((price, index) => (
+            <>
+              <li key={index} className={styles.priceItem}>
+                <p className={styles.ticketType}>{price.ticketType}</p>
+                <p className={styles.price}>{formatTicketPrice(price)}</p>
+              </li>
+              <div className={styles.spacer} />
+            </>
+          ))}
+        </ul>
+             <p className={styles.disclaimer}>
+        * This event may be 18+. Information presented on this page may be false or outdated.
+        Always check with the <a href={originalPostUrl}>official listing</a> before purchasing
+        tickets for any event.
+      </p>
       </div>
-      <ul className={styles.pricesList}>
-        {prices.map((price, index) => (
-          <li key={index} className={styles.priceItem}>
-            {formatTicketString(price)}
-          </li>
-        ))}
-      </ul>
+ 
     </div>
   )
 }
