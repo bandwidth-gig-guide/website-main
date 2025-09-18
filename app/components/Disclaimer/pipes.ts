8c2bc7edbb7e70e86543ad186c94d3d1c6dfdbf8
MatthewLeigh <matthewcross97@gmail.com>
2025-09-18 11:01:47 +1000

    feat: define communication methods

---
+++ app/components/Disclaimer/pipes.ts
@@ -0,0 +1,6 @@
+export const pipes = [
+    { label: "Discord", image: "/discord.svg", href: "https://google.com" },
+    { label: "Instagram", image: "/instagram.svg", href: "https://google.com" },
+    { label: "Facebook", image: "/facebook.svg", href: "https://google.com" },
+    { label: "Email", image: "/email.svg", text: "placeholder@email.com" },
+];
