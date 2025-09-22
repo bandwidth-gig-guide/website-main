4f929e312c7c2ddf3af4ef0f0979146dccabc3be
Matthew Cross <119134131+MatthewLeigh@users.noreply.github.com>
2025-09-22 10:06:04 +1000

    test: update apiUrl configuration to use local path

--- app/api.config.ts
+++ app/api.config.ts
@@ -1,4 +1,5 @@
 // const apiUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:80/public";
-const apiUrl = "https://bandwidth.melbourne/public";
+// const apiUrl = "https://bandwidth.melbourne/public";
+const apiUrl = "/public";
 
 export default apiUrl;
