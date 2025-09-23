1b0b561a55da2235af48a5b0c77378688ef4c7ee
Matthew Cross <119134131+MatthewLeigh@users.noreply.github.com>
2025-09-23 13:10:39 +1000

    test(temp): hardcode the production api url to test

--- app/next.config.js
+++ app/next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   publicRuntimeConfig: {
-    SERVICE_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:80/public",
+    SERVICE_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "https://bandiwdth.melbourne/public",
   },
 };
