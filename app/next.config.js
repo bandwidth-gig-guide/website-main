c5790f3ef771bb12d2e739c04691a9979c638092
Matthew Cross <119134131+MatthewLeigh@users.noreply.github.com>
2025-09-23 13:15:11 +1000

    test(temp): use http instead of https

--- app/next.config.js
+++ app/next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   publicRuntimeConfig: {
-    SERVICE_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "https://bandiwdth.melbourne/public",
+    SERVICE_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "http://bandiwdth.melbourne/public",
   },
 };
