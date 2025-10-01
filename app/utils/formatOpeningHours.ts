7cdb6441f756a3e77c4a79333e1861b0f8397368
Matthew Cross <119134131+MatthewLeigh@users.noreply.github.com>
2025-10-02 00:06:20 +1000

    refactor: improve readability of function

--- app/utils/formatOpeningHours.ts
+++ app/utils/formatOpeningHours.ts
@@ -2,23 +2,21 @@ export function formatOpeningHours(opening: string, closing: string): string {
 
     const timeRegex = /^\d{2}:\d{2}:\d{2}$/;
 
-    if (!timeRegex.test(opening) || !timeRegex.test(closing)) {
-        return 'unknown';
-    }
-
-    if (opening == closing) return 'closed'
-
-    const formatTime = (time: string) => {
-        const [hourStr, minuteStr] = time.split(':');
-        let hour = parseInt(hourStr, 10);
-        const minute = minuteStr;
-        const ampm = hour >= 12 ? 'pm' : 'am';
-        hour = hour % 12 === 0 ? 12 : hour % 12;
-        return `${hour}:${minute}${ampm}`;
-    };
+    if (!timeRegex.test(opening) || !timeRegex.test(closing)) return 'unknown';
+    if (opening == closing) return 'closed';
 
     const openingFormatted = formatTime(opening);
-    let closingFormatted = closing === '23:59:00' ? 'late' : formatTime(closing);
+    const closingFormatted = closing === '23:59:00' ? 'late' : formatTime(closing);
 
     return `${openingFormatted} - ${closingFormatted}`;
 }
+
+const formatTime = (time: string) => {
+    const [hourStr, minuteStr] = time.split(':');
+    let hour = parseInt(hourStr, 10);
+    const minute = minuteStr;
+    const ampm = hour >= 12 ? 'pm' : 'am';
+    hour = hour % 12 === 0 ? 12 : hour % 12;
+    
+    return `${hour}:${minute}${ampm}`;
+};
