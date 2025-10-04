b11862559fba1aad6d779e4753598a2a1867e74d
Matthew Cross <119134131+MatthewLeigh@users.noreply.github.com>
2025-10-05 00:00:27 +1000

    feat: add FILTER_CITIES_KEY to local storage constants

--- app/constants/localstorage.ts
+++ app/constants/localstorage.ts
@@ -1,5 +1,6 @@
-// All
+// Shared
 export const FILTER_TAGS_KEY = "filterTags";
+export const FILTER_CITIES_KEY = "filterCities";
 
 // Artist
 export const FILTER_ARTIST_TYPE_KEY = "filterArtistType";
@@ -11,7 +12,6 @@ export const FILTER_ARTIST_UPCOMING_KEY = "filterArtistHasUpcomingPerformance";
 // Event
 export const FILTER_EVENT_NAME_KEY = "filterEventName";
 export const FILTER_EVENT_STATECODE_KEY = "filterEventStateCode";
-export const FILTER_EVENT_CITY_KEY = "filterEventCity";
 export const FILTER_EVENT_MAX_PRICE_KEY = "filterEventMaxPrice";
 export const FILTER_EVENT_TYPE_KEY = "filterEventType";
 export const FILTER_EVENT_DATE_KEY = "filterEventDate";
@@ -19,6 +19,5 @@ export const FILTER_EVENT_DATE_KEY = "filterEventDate";
 // Venue
 export const FILTER_VENUE_NAME_KEY = "FilterVenueName";
 export const FILTER_VENUE_STATECODE_KEY = "FilterVenueStateCode";
-export const FILTER_VENUE_CITY_KEY = "FilterVenueCity";
 export const FILTER_VENUE_TYPE_KEY = "FilterVenueType";
 export const FILTER_VENUE_UPCOMING_KEY = "filterVenueHasUpcomingPerformance";
