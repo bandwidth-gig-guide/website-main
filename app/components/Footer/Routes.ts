614ea9708d9efd5b429da8a0aca81530be758245
Matthew Cross <119134131+MatthewLeigh@users.noreply.github.com>
2025-11-23 22:42:58 +1100

    feat: define external routes

--- app/components/Footer/Routes.ts
+++ app/components/Footer/Routes.ts
@@ -1,10 +1,13 @@
-export const routes = [
+export const internalRoutes = [
     { label: "Events", href: "/event" },
     { label: "Artists", href: "/artist" },
     { label: "Venues", href: "/venue" },
     { label: "About", href: "/about" },
-
-    // { label: "Articles", href: "/" },
-    // { label: "Teachers", href: "/" },
-    // { label: "Stores", href: "/" },
 ];
+
+export const externalRoutes = [
+    { label: "Facebook", href: "https://www.facebook.com/profile.php?id=61583501446966" , img: "facebook-white.svg"},
+    { label: "Instagram", href: "https://www.instagram.com/bandwidth.melbourne/" , img: "instagram-white.svg"},
+    { label: "Discord", href: "https://discord.com/channels/1417810337826340937/1417810338472267858" , img: "discord-white.svg"},
+    { label: "GitHub", href: "https://github.com/orgs/bandwidth-gig-guide/repositories" , img: "github-white.svg"},
+]
