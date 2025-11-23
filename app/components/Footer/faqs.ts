df4e06090c0ab1751ad7eb2c33e44b0d69a97c31
Matthew Cross <119134131+MatthewLeigh@users.noreply.github.com>
2025-11-23 22:43:15 +1100

    feat: define frequently asked questions

---
+++ app/components/Footer/faqs.ts
@@ -0,0 +1,14 @@
+export const faqs = [
+    { 
+      title: "What is Bandwidth?", 
+      content: "Bandwidth is the definitive Melbourne Gig Guide website that let's you search for upcoming events, local venues, and your favorite artists." 
+    },
+    { 
+      title: "How do we decide which artists to include?", 
+      content: "Artists are added to our database when they are first identified as performing a gig at a venue in Melbourne. If your favorite artist isn't on the site yet, tell them to start playing some gigs in Melbourne!" 
+    },
+     { 
+      title: "Why can't I see my favorite venue on here?", 
+      content: "Gathering information takes time! The more venues we track, the long it takes each week. We're growing slowly, and making sure to only add more venues when we're confident that we can handle the load." 
+    },
+]
